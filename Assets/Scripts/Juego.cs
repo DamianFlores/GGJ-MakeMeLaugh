7d4a06303cdecc9c4526a54af41769c0e1c891aa
Damián <damian.flores.rf@gmail.com>
2024-01-28 18:02:43 -0300

    Final?

--- Assets/Scripts/Juego.cs
+++ Assets/Scripts/Juego.cs
@@ -14,6 +14,10 @@ public class Juego : MonoBehaviour
 
     public int graciaFinal, indignacionFinal;
 
+    public GameObject cartelVictoria, cartelDerrota;
+
+    private bool terminado;
+
     void Start()
     {
         Instancia = this;
@@ -34,6 +38,9 @@ public class Juego : MonoBehaviour
         {
             puntajeGracia = value;
             uiGracia.Valor = 1f * puntajeGracia / graciaFinal;
+
+            if (puntajeGracia > graciaFinal && !terminado)
+                Ganar();
         }
         get => puntajeGracia;
     }
@@ -44,7 +51,23 @@ public class Juego : MonoBehaviour
         {
             puntajeIndignacion = value;
             uiIndignacion.Valor = 1f * puntajeIndignacion / indignacionFinal;
+
+
+            if (puntajeIndignacion > indignacionFinal && !terminado)
+                Perder();
         }
         get => puntajeIndignacion;
+    }
+
+    private void Ganar()
+    {
+        cartelVictoria.SetActive(true);
+        terminado = true;
+    }
+
+    private void Perder()
+    {
+        cartelDerrota.SetActive(true);
+        terminado = true;
     }
 }
